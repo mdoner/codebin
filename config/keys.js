bd96f7cd2d8d249ac1fade54f1ea438223084568
ntaheij <noah@taheij.nl>
2020-09-16 15:21:45 +0200

    added key file

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+dbPassword =
+	'mongodb+srv://codebin:' +
+	encodeURIComponent('xP9akxopxRm2t4lH') +
+	'@codebin-db.uckpf.azure.mongodb.net/codebin?retryWrites=true';
+
+module.exports = {
+	mongoURI: dbPassword
+};
